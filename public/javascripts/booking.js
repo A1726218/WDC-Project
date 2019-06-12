a788eaf5f9562e7f64e68c1f540692237b9faef4
Wesley <wesleytws2018@gmail.com>
2019-06-12 22:07:43 +0930

    Merge

--- public/javascripts/booking.js
+++ public/javascripts/booking.js
@@ -1,6 +1,40 @@
+var map;
+
 function initMap() {
-    new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
+
+map = new google.maps.Map(document.getElementById('map'), {
+center: {lat: -34.397, lng: 150.644},
+zoom: 8
+});
+}
+
+function imageClick(url) {
+    window.location = url;
+}
+
+function submitBooking(){
+	var guests = document.getElementById("numberGuests").value;
+	if (guests < 1 || guests > 30){
+	alert('Invalid amount of guests, must be greater than 0');
+	}
+	var time = document.getElementById("startTime").value;
+	if (time < "09:30" || time > "21:00"){
+	//alert("Invalid time input, check open times");
+	}
+	var currentDate = newDate();
+    /*var day = currentDate.getDate();
+    var month = currentDate.getMonth()+1; //January is 0
+    var year = currentDate.getFullYear();
+    if(day<10){
+    	day='0'+day
+    } 
+    if(month<10){
+       month='0'+month
+    }
+     currentDate = year+'-'+month+'-'+day;
+	document.getElementById("selectedDate").setAttribute("min",currentDate);*/
+	//var bookingDate = document.getElementById("selectedDate").value;
+	//if(!bookingDate){
+	//alert("hello");
+//}
 }
